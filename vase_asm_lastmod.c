96789ec1111b789fbb90f6d8165e5170858c1756
roxaw <107312257+roxaw@users.noreply.github.com>
2024-12-01 12:46:52 -0600

    Update vase_asm_lastmod.c

--- vase_asm_lastmod.c
+++ vase_asm_lastmod.c
@@ -1,3 +1,4 @@
+//Modified stub for atoi
 //Modified Code to Replace Symbolic Variables with Fixed Values:void __VERIFIER_nondet_u32
 
 //vase modified+ new definition for string_equals
@@ -29,7 +30,22 @@ void klee_id(const char *message) {
 
 // Stub for atoi
 int atoi(const char *str) {
-    return 1; 
+    int res = 0; // Initialize result
+    int sign = 1; // Initialize sign as positive
+    int i = 0; // Initialize index of first digit
+
+    // If number is negative, then update sign
+    if (str[0] == '-') {
+        sign = -1;
+        i++; // Also update index of first digit
+    }
+
+    // Iterate through all digits of input string and update result
+    for (; str[i] != '\0'; ++i)
+        res = res * 10 + str[i] - '0';
+
+    // Return result with sign
+    return sign * res;
 }
 
 // Stub for printf
